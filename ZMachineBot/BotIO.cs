29b9f315a895bca54d70bed481d03d25f71eee5d
Brian Peek <brian@brianpeek.com>
2016-09-05 01:05:46 -0400

    Initial import

---
+++ ZMachineBot/BotIO.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using System.Text;
+using ZMachineLib;
+
+namespace ZMachineBot
+{
+	public class BotIO : IZMachineIO
+	{
+		private readonly StringBuilder _sb = new StringBuilder();
+
+		public string Text => _sb.ToString().Replace("\r\n", "\r\n\r\n");
+
+		public void Print(string s)
+		{
+			_sb.Append(s);
+		}
+
+		public string Read(int max)
+		{
+			return null;
+		}
+
+		public char ReadChar()
+		{
+			return '\0';
+		}
+
+		public void SetCursor(ushort line, ushort column, ushort window)
+		{
+		}
+
+		public void SetWindow(ushort window)
+		{
+		}
+
+		public void EraseWindow(ushort window)
+		{
+		}
+
+		public void BufferMode(bool buffer)
+		{
+		}
+
+		public void SplitWindow(ushort lines)
+		{
+		}
+
+		public void ShowStatus()
+		{
+		}
+
+		public void SetTextStyle(TextStyle textStyle)
+		{
+		}
+
+		public bool Save(Stream stream)
+		{
+			return false;
+		}
+
+		public Stream Restore()
+		{
+			return null;
+		}
+
+		public void SetColor(ZColor foreground, ZColor background)
+		{
+		}
+
+		public void SoundEffect(ushort number)
+		{
+		}
+
+		public void Quit()
+		{
+		}
+	}
+}
---
+++ ZMachineBot/Controllers/MessagesController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using System.Web.Http;
+using Microsoft.Bot.Connector;
+using ZMachineLib;
+
+namespace ZMachineBot.Controllers
+{
+	[BotAuthentication]
+	public class MessagesController : ApiController
+	{
+		/// <summary>
+		/// POST: api/Messages
+		/// Receive a message from a user and reply to it
+		/// </summary>
+		public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+		{
+			BotIO io = new BotIO();
+			ZMachine zMachine = new ZMachine(io);
+
+			string path = HostingEnvironment.MapPath("~/Games/zork1.dat");
+			FileStream fs = File.OpenRead(path);
+			zMachine.LoadFile(fs);
+
+			BotData data = activity.GetStateClient().BotState.GetUserData(activity.ChannelId, activity.From.Id);
+			byte[] state = data.GetProperty<byte[]>("ZState");
+			if(state != null)
+			{
+				using (MemoryStream ms = new MemoryStream(state))
+					zMachine.RestoreState(ms);
+				zMachine.FinishRead(activity.Text);
+			}
+
+			zMachine.Run(true);
+			Stream s = zMachine.SaveState();
+			using (MemoryStream ms = new MemoryStream())
+			{
+				s.CopyTo(ms);
+				data.SetProperty("ZState", ms.ToArray());
+			}
+			activity.GetStateClient().BotState.SetUserData(activity.ChannelId, activity.From.Id, data);
+
+			if (activity.Type == ActivityTypes.Message)
+			{
+				ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+				// return our reply to the user
+				Activity reply = activity.CreateReply(io.Text);
+				await connector.Conversations.ReplyToActivityAsync(reply);
+			}
+			else
+			{
+				HandleSystemMessage(activity);
+			}
+			var response = Request.CreateResponse(HttpStatusCode.OK);
+			return response;
+		}
+
+		private Activity HandleSystemMessage(Activity message)
+		{
+			if (message.Type == ActivityTypes.DeleteUserData)
+			{
+				// Implement user deletion here
+				// If we handle user deletion, return a real message
+			}
+			else if (message.Type == ActivityTypes.ConversationUpdate)
+			{
+				// Handle conversation state changes, like members being added and removed
+				// Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+				// Not available in all channels
+			}
+			else if (message.Type == ActivityTypes.ContactRelationUpdate)
+			{
+				// Handle add/remove from contact lists
+				// Activity.From + Activity.Action represent what happened
+			}
+			else if (message.Type == ActivityTypes.Typing)
+			{
+				// Handle knowing tha the user is typing
+			}
+			else if (message.Type == ActivityTypes.Ping)
+			{
+			}
+
+			return null;
+		}
+	}
+}
